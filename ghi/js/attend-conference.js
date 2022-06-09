fc75b5f53e1b5d6b2a76d625d6b78ec0677211d2
Morgan Lyle <morganlylee@gmail.com>
2022-06-09 14:58:02 -0500

    Adding the attendee

--- ghi/js/attend-conference.js
+++ ghi/js/attend-conference.js
@@ -1,7 +1,8 @@
 window.addEventListener('DOMContentLoaded', async () => {
     const selectTag = document.getElementById('conference');
     const loadingIcon = document.getElementById("loading-conference-spinner");
-  
+    const successTag = document.getElementById("success-message")
+
     const url = 'http://localhost:8000/api/conferences/';
     const response = await fetch(url);
     if (response.ok) {
@@ -17,6 +18,32 @@ window.addEventListener('DOMContentLoaded', async () => {
    
       loadingIcon.classList.add('d-none');
       selectTag.classList.remove('d-none');
+
+      const formTag = document.getElementById('create-attendee-form');
+    formTag.addEventListener('submit', async event => {
+      event.preventDefault();
+      const formData = new FormData(formTag);
+      const json = JSON.stringify(Object.fromEntries(formData));
+
+      const attendeesUrl = 'http://localhost:8001/api/attendees/';
+      const fetchConfig = {
+        method: "post",
+        body: json,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+};      
+       const response = await fetch(attendeesUrl, fetchConfig);
+
+         if (response.ok) {
+         formTag.reset();
+         const newConference = await response.json();
+         successTag.classList.remove('d-none');
+
+         console.log(newConference);
+}
+      
+});
     }
    
 
