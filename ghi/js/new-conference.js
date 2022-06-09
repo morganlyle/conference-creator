38c5ee51173695ba0757e3fe14d6c2f355c06c07
Morgan Lyle <morganlylee@gmail.com>
2022-06-08 20:44:06 -0500

    new conferences

---
+++ ghi/js/new-conference.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', async () => {
+
+    const url = 'http://localhost:8000/api/locations/';
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error('Response not ok');
+        } else {
+            const data = await response.json();
+            const locationTag = document.getElementById('location');
+            console.log(data)
+            for (let loct of data.locations) {
+                const secondOption = document.createElement('option');
+                secondOption.value = loct.id;
+                secondOption.innerHTML = loct.name;
+                locationTag.appendChild(secondOption)
+                console.log("STUFF:", loct)
+            }
+            const formTag = document.getElementById('create-conference-form')
+            formTag.addEventListener('submit', async event => {
+                event.preventDefault();
+                const formData = new FormData(formTag);
+                const json = JSON.stringify(Object.fromEntries(formData));
+
+                const locationUrl = 'http://localhost:8000/api/conferences/';
+                const fetchConfig = {
+                  method: "post",
+                  body: json,
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                };
+                console.log(json)
+                const response = await fetch(locationUrl, fetchConfig);
+                console.log("THINGS12:", response)
+                if (response.ok) {
+                  formTag.reset();
+                  const newLocation = await response.json(); }
+})}
+    } catch (e) {
+        console.error('error', e);
+    }
+});
--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -1,34 +1,48 @@
 
 window.addEventListener('DOMContentLoaded', async () => {
     const url = 'http://localhost:8000/api/states/'
-
     const response = await fetch(url);
-    
     if (response.ok) {
         const data = await response.json();
-       
-        
     // Get the select tag element by its id 'state'
         const selectTag = document.getElementById('state')
     // For each state in the states property of the data
         for (let state of data.states){
-
-        
     // Create an 'option' element
             const option = document.createElement('option')
     // Set the '.value' property of the option element to the
     // state's abbreviation
             option.value = state.abbreviation
-            option.innerHTML = state.name
-    // Set the '.innerHTML' property of the option element to
+                // Set the '.innerHTML' property of the option element to
     // the state's name
-
+            option.innerHTML = state.name
     // Append the option element as a child of the select tag
             selectTag.appendChild(option)
-
-        }
-
+            console.log()
     }
+    
+    const formTag = document.getElementById('create-location-form');
+    formTag.addEventListener('submit', async event => {
+      event.preventDefault();
+      const formData = new FormData(formTag);
+      const json = JSON.stringify(Object.fromEntries(formData));
 
+      const locationUrl = 'http://localhost:8000/api/locations/';
+      const fetchConfig = {
+        method: "post",
+        body: json,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+};      console.log(json)
+       const response = await fetch(locationUrl, fetchConfig);
 
-})
+         if (response.ok) {
+         formTag.reset();
+         const newLocation = await response.json();
+         console.log(newLocation);
+}
+      
+});
+
+}});
